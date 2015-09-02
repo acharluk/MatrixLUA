788be9e409036080bdc9116427d443972f3671a3
ACharLuk <acharluk@gmail.com>
2015-09-02 13:09:16 +0200

    Error handling for main function

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 require "Matrix"
 
-matrix1 = Matrix:new({{0}}) --1x1 matrix
-print("Rows: " .. matrix1.rows)
-print("Columns" .. matrix1.columns)
-print(matrix1.matrix[1][1])
+function main()
+	matrix1 = Matrix:new({{0}}) --1x1 matrix
+	print("Rows: " .. matrix1.rows)
+	print("Columns: " .. matrix1.columns)
+	print(matrix1.matrix[1][1])
+end
+
+local ok, err = pcall(main)
+if not ok then
+	print("Something went wrong :(")
+	print(err)
+end
