2d59ce9cacfb052c2ba47c6585f3d2295f3be5cd
ACharLuk <acharluk@gmail.com>
2015-09-03 09:37:32 +0200

    Simple matrix adding

--- Matrix.lua
+++ Matrix.lua
@@ -7,5 +7,15 @@ function Matrix:new(matrix)
 	self.rows = #matrix
 	self.columns = #matrix[1]
 
+	self.add = function(self, mat)
+		result = Matrix:new(self.matrix) --This sets the matrix size, data will be overwritten
+		for i = 1, self.rows do
+			for j = 1, self.columns do
+				result.matrix[i][j] = self.matrix[i][j] + mat.matrix[i][j]
+			end
+		end
+		return result
+	end
+
 	return self
 end
--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 require "Matrix"
 
 function main()
-	matrix1 = Matrix:new({{0}}) --1x1 matrix
-	print("Rows: " .. matrix1.rows)
-	print("Columns: " .. matrix1.columns)
-	print(matrix1.matrix[1][1])
+	local matrix1 = Matrix:new({{0}}) --1x1 matrix
+	local matrix2 = Matrix:new({{0, 1}, {1, 0}}) --2x2 matrix
+	local matrix3 = Matrix:new({{1, 2}, {3, 4}}) --Another 2x2 matrix
+	print(matrix3:add(matrix2))
 end
 
 local ok, err = pcall(main)
